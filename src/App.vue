366c6d26e51b2f7da3c879d9cd605f836b545f1c
MariaDiezEsteve <diezesteve@outlook.es>
2023-03-06 22:17:56 +0100

    incluido directivas v-for, v-if, evento click

--- src/App.vue
+++ src/App.vue
@@ -12,39 +12,83 @@
   <p v-else-if="decision === false">La descisión es falsa</p>
   <p v-else>La decisión es indecisa</p>
 
+  <span v-show="seMuestra"> Lorem ipsum dolor sit amet.</span>
+  <!-- Esto será display:block cuando seMuestra sea true, y será display:none cuando seMuestra sea false -->
+
+  <h3>Fruit List</h3>
+  <ul>
+    <li v-for = "(fruta , index) in arrayFrutas" :key="index"> {{ index  }} - {{ fruta }}   </li>
+  </ul>
+
+  <h3>Information about fruits</h3>
+  <ul>
+    <li v-for="(fruit, index) in arrayFruits" :key="fruit.name">
+      <span v-if="fruit.stock > 20">  {{ index }} - {{ fruit.name }} - {{ fruit.price }} - {{ fruit.description }}</span>
+   
+    </li>
+  </ul>
+
+
+  <h3>Information about one fruit</h3>
+  <ul>
+    <li v-for="value in objetoFruta" :key="value">
+      {{ value }}
+    </li>
+  </ul>
+
+  <button @click="handleClick">Click aquí</button>
+
 </template>
 
-<!-- <script setup>
+<script setup>
+
 const name = "Vue dinámico"
 const styleColor = "color: blue"
 const arrayColores = ["blue", "red", "peru"]
 const activo = false;
 const decision = null;
+const seMuestra = true
 
-</script> -->
+const arrayFrutas = ["cherry", "peach", "banana", "pineapple", "strawberry"];
 
-<script>
-export default {
+const arrayFruits = [
+  {
+    name: "Mango",
+    price: "2€",
+    description: "El mango se vende por unidad",
+    stock: 0
 
-  setup(){
-    const name = "Vue dinámico"
-    const styleColor = "color: blue"
-    const arrayColores = ["blue", "red", "peru"]
-    const activo = false;
-    const decision = null;
-    
-    return {
-      name,
-      styleColor,
-      arrayColores,
-      activo,
-      decision
-    }
+  },
+  {
+    name: "Orange",
+    price: "1.5€",
+    description: "Orange se vende por kilos",
+    stock: 40
+
+  },
+  {
+    name: "Kiwi",
+    price: "2.9€",
+    description: "Kiwi se vende por kilos",
+    stock: 60
+
   }
-}
+]
+
+const objetoFruta = {
+  id: 1,
+  name: "grapes",
+  price: "5€",
+  description:"A mount of grapes"
+
+};
 
 
-</script>
+const  handleClick = () => {
+  console.log("Me diste click")
+};
+</script> 
+
 
 <style>
 
